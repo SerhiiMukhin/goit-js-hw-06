868085b7a72223ddea464c6b15b26f0ae0ddcb30
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-09 18:28:07 +0200

    task 2 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+for (const ingredient of ingredients) {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  list.append(item);
+}
