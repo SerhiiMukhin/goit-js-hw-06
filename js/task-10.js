242069fd32763a75b6d1a0cc5201c78ef5b6d0f4
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-11 18:45:04 +0200

    task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+
+const createBtn = document.querySelector('button[data-create]');
+
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+const boxContainer = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  amount = input.value;
+  for (let index = 0; index < amount; index += 1) {
+    const div = document.createElement("div")
+    div.classList.add("createdDiv")
+    boxContainer.append(div);
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${30 + 10*index}px`;
+    div.style.height = `${30 + 10*index}px`;
+  }
+}
+
+function destroyBoxes(amount) {
+  amount = boxContainer.childElementCount;
+for (let index = 0; index < amount; index += 1) {
+  const divToRemove = document.querySelector(".createdDiv")
+  divToRemove.remove()
+}
+}
+
+createBtn.addEventListener("click", createBoxes)
+destroyBtn.addEventListener("click", destroyBoxes)
+
