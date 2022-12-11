0be8a7718eaf8ca98828a41a9e688f77f3242e0e
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-11 14:56:10 +0200

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,54 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+    const {
+        elements: {email, password}
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert ("Please fill in all the fields!");
+    }
+
+const result = {
+    email: email.value,
+    password: password.value,
+};
+
+    console.log(result)
+
+    event.currentTarget.reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password }
+//   } = event.currentTarget;
+
+//   if (login.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
 
