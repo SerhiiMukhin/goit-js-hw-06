c9851324f05d3be51dbdf6b1fd4225e2d3302c32
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-11 15:23:14 +0200

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+
+const text = document.querySelector('.color');
+
+const changeColorBtn = document.querySelector('.change-color');
+
+function changeBodyColor() {
+    body.style.backgroundColor = getRandomHexColor()
+    text.textContent = getRandomHexColor()
+}
+
+
+changeColorBtn.addEventListener("click", changeBodyColor);
