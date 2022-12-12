cd948c4ebb10a887676e0f703b32a78ca6340b95
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2022-12-12 19:14:31 +0200

    task 2 fix

--- js/task-02.js
+++ js/task-02.js
@@ -2,9 +2,11 @@ const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Con
 
 const list = document.querySelector('#ingredients');
 
-for (const ingredient of ingredients) {
+const markup = ingredients.map((ingredient) => {
   const item = document.createElement('li');
   item.textContent = ingredient;
   item.classList.add('item');
-  list.append(item);
-}
+  return item
+  })
+
+  list.append(...markup);
